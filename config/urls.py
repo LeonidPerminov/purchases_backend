eda1c2961b644d57e34798c004322bc3c510d6df
Leonid Perminov <lperminov@mail.ru>
2025-12-12 11:18:42 +0300

    обавлены маршруты OAuth для авторизации через социальные сети

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from shop.views import RegisterView, ProductInfoListView  # ← добавили ProductInfoListView
+from shop.views import RegisterView, ProductInfoListView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,9 +22,30 @@ urlpatterns = [
     path('api/v1/products-info/', ProductInfoListView.as_view(), name='products-info'),
 
     # Все остальные эндпоинты из приложения shop (магазины, категории, товары, заказы, контакты)
-    path('api/v1/', include('shop.urls')),  # ← ОСТАВЛЯЕМ ТОЛЬКО ОДНУ СТРОКУ
+    path('api/v1/', include('shop.urls')),
 
     # Логин/логаут для DRF Browsable API
     path('api-auth/', include('rest_framework.urls')),
+
+    # ----- DRF Spectacular -----
+
+    # OpenAPI schema (JSON/YAML)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI
+    path(
+        'api/schema/swagger/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
+
+    # ReDoc
+    path(
+        'api/schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc',
+    ),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
+
 
